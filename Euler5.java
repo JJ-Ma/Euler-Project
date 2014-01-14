ce4de4ca2299d4911c488007fd110a360625d231
soeb003 <bsoe@ucsd.edu>
2014-01-14 01:00:01 -0800

    Problem 5

--- Euler5.java
+++ Euler5.java
@@ -1,16 +1,16 @@
 public class Euler5
 {
   public static void main(String[] args)
-	{
-	  int value = 1;
-		for(int i = 1; i <= 10; i++)
-		{
-		  if(value%i != 0)
-			{
-			  value++;
-				i = 1;
-			}
-		}
-		System.out.println(value);
-	}
+  {
+    int value = 1;
+    for(int i = 1; i <= 10; i++)
+    {
+      if(value%i != 0)
+      {
+        value++;
+        i = 1;
+      }
+    }
+    System.out.println(value);
+  }
 }
