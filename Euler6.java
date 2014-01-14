ad9c0837f22f6030d9834624dba2dfe7e6189bea
soeb003 <bsoe@ucsd.edu>
2014-01-14 00:58:23 -0800

    Problem 6

---
+++ Euler6.java
@@ -0,0 +1,29 @@
+public class Euler6
+{
+  public int SumOfSquares(int min, int max)
+  {
+    int sum = 0;
+    while(min <= max)
+    {
+      sum = sum + (min*min);
+      min++;
+    }
+    return sum;
+  }
+
+  public int SquareOfSum(int min, int max)
+  {
+    int sum = 0;
+		while(min <= max)
+    {
+      sum = sum + min;
+      min++;
+    }
+    return sum*sum;
+  }
+  public static void main(String[] args)
+  {
+    Euler6 answer = new Euler6();
+    System.out.println(answer.SumOfSquares(1,10)-answer.SquareOfSum(1,10));
+  }
+}
